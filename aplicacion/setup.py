73fe6d03d74ca543c1630efd792055589311f655
srmf9 <salviwui@gmail.com>
2015-10-28 17:58:30 +0100

    actualizando

--- aplicacion/setup.py
+++ aplicacion/setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(name='Encuestas',
 
       version='1.0',
-      description='Aplicación web sobre encuestas',
+      description='Aplicacion web sobre encuestas',
       author='Salvador Rueda Molina',
       author_email='salviwui@gmail.com',
       url='enlace',
-      packages=['Encuestas'],
+      packages=['aplicacion'],
       install_requires=['django', 'wheel'],
      )
