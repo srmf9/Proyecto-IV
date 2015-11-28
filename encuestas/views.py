7719241545baee68f010e1121e5a6e40fd4c5e13
srmf9 <salviwui@gmail.com>
2015-11-28 19:08:05 +0100

    restructurando

---
+++ encuestas/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'encuestas/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Devuelve los últimos cinco preguntas publicadas."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'encuestas/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'encuestas/results.html'
+
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def encuestas(request):
+
+    if request.method == 'GET':
+        encuestas = Question.objects.all()
+        serializador = QuestionSerializer(encuestas, many=True)
+        return JSONResponse(serializador.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializador = QuestionSerializer(data=data)
+        if serializador.is_valid():
+            serializador.save()
+            return JSONResponse(serializador.data, status=201)
+    return JSONResponse(serializador.errors, status=400)
+
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        """ vuelve a mostrar la votacion """
+        return render(request, 'encuestas/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+	""" Devuelve HttpResponseRedirect cuando la accion ocurre con exito """
+        return HttpResponseRedirect(reverse('results', args=(p.id,)))
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='Encuestas',
+
+      version='1.0',
+      description='Aplicacion web sobre encuestas',
+      author='Salvador Rueda Molina',
+      author_email='salviwui@gmail.com',
+      url='enlace',
+      packages=['aplicacion'],
+      install_requires=['django', 'wheel'],
+     )
