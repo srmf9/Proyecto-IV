b8f2b41eb81badea17a17b0cb6e5563b17f8f63f
srmf9 <salviwui@gmail.com>
2015-11-29 19:07:28 +0100

    arreglando aplicacion 2

--- encuestas/urls.py
+++ encuestas/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     url(r'^encuestas/$', views.encuestas)
+    url(r'^$', views.encuestas)
 ]
