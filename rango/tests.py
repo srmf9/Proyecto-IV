04e7f10fce88a9d27a62d096a1d80a4321bfe6e2
srmf9 <salviwui@gmail.com>
2016-02-05 17:07:56 +0100

    incorporando test

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Bares,Tapas
+
+class TestBar(TestCase):
+
+	def test_crear_bar(self):
+		bar = Bares(nombre="bar", direccion="direccion",visitas=1)
+		bar.save()
+		self.assertEqual(bar.nombre, "bar")
+		print("Bar creado correctamente")
